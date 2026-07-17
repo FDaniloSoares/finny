caa9fe58294fc59373be65c20a92fd5fa1e74c7f
FDaniloSoares <fdanilosoares@gmail.com>
2026-07-16 22:33:09 -0300

    chore: configure biome to ordenate css classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Inicial</div>;
+  return (
+    <div className="mt-4 flex items-center rounded bg-blue-500 p-4 text-red-500">
+      Inicial
+    </div>
+  );
 }
